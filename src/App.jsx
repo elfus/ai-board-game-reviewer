12ea4908d240ed9109c86a1fc6e31ab0b3e8907b
Adrian Ortega <adrianog.sw@gmail.com>
2024-08-04 14:37:00 -0600

    Deleted App.css and reformatted double quotes with single quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import "./App.css";
 import {
   createBrowserRouter,
   Navigate,
   RouterProvider,
-} from "react-router-dom";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
-import Home from "./ui/Home";
-import BoardGameList from "./ui/BoardGameList";
-import BoardGame from "./ui/BoardGame";
+} from 'react-router-dom';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
+import Home from './ui/Home';
+import BoardGameList from './ui/BoardGameList';
+import BoardGame from './ui/BoardGame';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
     children: [
-      { path: "/", element: <Home />, errorElement: <Error /> },
-      { path: "/home", element: <Navigate to="/" replace /> },
+      { path: '/', element: <Home />, errorElement: <Error /> },
+      { path: '/home', element: <Navigate to="/" replace /> },
       {
-        path: "/list",
+        path: '/list',
         element: <BoardGameList />,
         errorElement: <Error />,
       },
       {
-        path: "/game/:gameId",
+        path: '/game/:gameId',
         element: <BoardGame />,
         errorElement: <Error />,
       },
