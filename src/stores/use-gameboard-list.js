ffa794a59c20e3acb06421afc9be93901b9691d5
Sergio Guerra <sergioguerra1991@gmail.com>
2024-08-06 06:32:02 -0600

    Allow https

--- src/stores/use-gameboard-list.js
+++ src/stores/use-gameboard-list.js
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-const BASE_API_URL = `http://${window.location.hostname}`;
+const BASE_API_URL = `https://${window.location.hostname}`;
 //const BASE_API_URL = `http://${window.location.hostname}:3000`;
 
 function sumScore(score, rating) {
