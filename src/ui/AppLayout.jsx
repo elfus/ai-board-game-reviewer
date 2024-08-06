ef6701668d2903eede9dede525d902f57c838716
Sergio Guerra <sergioguerra1991@gmail.com>
2024-08-05 21:39:37 -0600

    UI theme changes

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,10 +4,13 @@ import Footer from './Footer';
 
 function AppLayout() {
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      <Header />
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-slate-800">
+      <video src="./BG.mov" autoPlay={true} muted={true} loop={true} className="absolute z-0 w-screen top-0 left-0 opacity-30">
+        <source src="./BG.mov" type="video/mp4" />
+      </video>
+      <Header className="z-10"/>
       {/* TODO: Fix the usage of class overflow-scroll */}
-      <div className="overflow-hidden">
+      <div className="overflow-hidden z-10">
         <main className="mx-auto">
           <Outlet />
         </main>
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,22 +1,17 @@
-import SearchBoardGame from '../features/boardgame/SearchBoardGame';
 import Button from './Button';
 
 function Home() {
   return (
     <div className="my-10 px-4 text-center">
-      <h1 className="my-10 px-4 text-xl font-semibold md:text-4xl">
-        The AI Board Game Reviewer
+      <h1 className="my-10 px-4 text-xl font-semibold md:text-4xl text-white">
+        The AI Game Board Reviewer
         <br />
-        <span className="text-xl text-amber-600">
+        <span className="text-xl text-teal-400">
           <p className="my-2 italic">
             Ratings based on the people&apos;s comments found on the internet.
           </p>
         </span>
       </h1>
-
-      <div className="my-10">
-        <SearchBoardGame />
-      </div>
 
       <Button type="primary" to="/list">
         See full list
