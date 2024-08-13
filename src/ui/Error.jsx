5d2430923a72cb1563b63e53d32182e60d943868
elfus <elfus0.1@gmail.com>
2024-08-13 11:19:15 -0600

    Improved error message.

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -5,10 +5,14 @@ function Error() {
   const error = useRouteError();
 
   return (
-    <div>
-      <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
-      <button onClick={() => navigate(-1)}>Go back</button>
+    <div className="flex place-content-center place-items-center h-screen ">
+      <div className="grid grid-rows-3 ">
+        <div className="border-2 font-bold border-amber-300 border-solid rounded-md bg-red-200 text-center">
+          <span className="text-3xl">Something went wrong 😢</span>
+          <p className="text-lg my-2">{error.data || error.message}</p>
+        </div>
+        <button className="mt-8 font-bold border-2 border-amber-300 border-solid rounded-full outline-8 bg-blue-500" onClick={() => navigate(-1)}>Go back</button>
+      </div>
     </div>
   );
 }
