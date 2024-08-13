6465644f592249ea033ff09245172e27dea2eccc
Adrian Ortega <adrianog.sw@gmail.com>
2024-08-12 18:22:00 -0600

    Fixed eslint errors

--- src/scripts/comments.js
+++ src/scripts/comments.js
@@ -1,25 +1,31 @@
-import gamelist from '../api/gameboard-list.json' assert { type: "json" };
+import gamelist from '../api/gameboard-list.json';
 
-for(let game of gamelist.games){
-    // Comments
-    let comments = game.comments ?? []
-    try {
-        const COMMENTS_API_URL = `https://api.geekdo.com/api/collections?ajax=1&comment=1&objectid=${game.objectId}&objecttype=thing&oneperuser=1&pageid=1&rated=1&require_review=true&showcount=50&sort=review_tstamp`
-        const commentsResponse = await fetch(COMMENTS_API_URL, { signal: AbortSignal.timeout(30000) }).then(res => res.json())
-        game.comments = (commentsResponse?.items ?? comments).map(item=>item?.textfield?.comment?.rendered??'').filter(str=> str.length > 25 && str.length < 300).slice(15)
-    }catch (e) {
-        game.comments = comments
-    }
-    // Price
-    let price = 0
-    try {
-        const PRICE_API_URL = `https://api.geekdo.com/api/geekbay/summary?nosession=1&objectid=${game.objectId}&objecttype=thing&sort=price`
-        const priceResponse = await fetch(PRICE_API_URL, { signal: AbortSignal.timeout(30000) }).then(res => res.json())
-        game.price = Number(priceResponse?.fromprice ?? price)
-    } catch (e) {
-        game.price = price
-    }
+for (let game of gamelist.games) {
+  // Comments
+  let comments = game.comments ?? [];
+  try {
+    const COMMENTS_API_URL = `https://api.geekdo.com/api/collections?ajax=1&comment=1&objectid=${game.objectId}&objecttype=thing&oneperuser=1&pageid=1&rated=1&require_review=true&showcount=50&sort=review_tstamp`;
+    const commentsResponse = await fetch(COMMENTS_API_URL, {
+      signal: AbortSignal.timeout(30000),
+    }).then((res) => res.json());
+    game.comments = (commentsResponse?.items ?? comments)
+      .map((item) => item?.textfield?.comment?.rendered ?? '')
+      .filter((str) => str.length > 25 && str.length < 300)
+      .slice(15);
+  } catch (e) {
+    game.comments = comments;
+  }
+  // Price
+  let price = 0;
+  try {
+    const PRICE_API_URL = `https://api.geekdo.com/api/geekbay/summary?nosession=1&objectid=${game.objectId}&objecttype=thing&sort=price`;
+    const priceResponse = await fetch(PRICE_API_URL, {
+      signal: AbortSignal.timeout(30000),
+    }).then((res) => res.json());
+    game.price = Number(priceResponse?.fromprice ?? price);
+  } catch (e) {
+    game.price = price;
+  }
 }
 
-console.log(JSON.stringify(gamelist, null, 4))
-
+console.log(JSON.stringify(gamelist, null, 4));
--- src/scripts/score.js
+++ src/scripts/score.js
@@ -1,24 +1,24 @@
 import { ollama } from 'ollama-ai-provider';
 import { generateObject } from 'ai';
-import { z } from 'zod'; 
+import { z } from 'zod';
 
-import gamelist from '../api/gameboard-list-comments.json' assert { type: "json" };
+import gamelist from '../api/gameboard-list-comments.json';
 
-for(let game of gamelist.games){
-    try{
-        const { object } = await generateObject({
-            model: ollama("llama3"),
-            mode: 'json',
-            schema: z.object({
-                fun: z.number(),
-                learning_curve: z.number(),
-                duration: z.number(),
-                players: z.number(),
-                difficulty: z.number(),
-                cost: z.number(),
-            }),
-            temperature: 0.5,
-            prompt: `Give me scores between 1 and 10 for ONLY the following categories (being 0 the worst score and 10 the best score): fun, learning_curve, duration, players, difficulty, cost.
+for (let game of gamelist.games) {
+  try {
+    const { object } = await generateObject({
+      model: ollama('llama3'),
+      mode: 'json',
+      schema: z.object({
+        fun: z.number(),
+        learning_curve: z.number(),
+        duration: z.number(),
+        players: z.number(),
+        difficulty: z.number(),
+        cost: z.number(),
+      }),
+      temperature: 0.5,
+      prompt: `Give me scores between 1 and 10 for ONLY the following categories (being 0 the worst score and 10 the best score): fun, learning_curve, duration, players, difficulty, cost.
                 Default to 1 if you don't rely on the answer of any of the categories.
                 Include score for all the categories even if you don't rely on the answer of any of the categories.
                 What are the scores for the game: "${game.name}"?, 
@@ -28,19 +28,19 @@ for(let game of gamelist.games){
                 the rating score is: ${game.rating} with number of votes: ${game.votes},
                 the price is: ${game.price},
                 and the comments provided: "${game.comments.join(',')}".`,
-        })
+    });
 
-        game.score = object
-    }catch(e){
-        game.score = {
-            fun: 1,
-            learning_curve: 1,
-            duration: 1,
-            players: 1,
-            difficulty: 1,
-            cost: 1,
-        }
-    }
+    game.score = object;
+  } catch (e) {
+    game.score = {
+      fun: 1,
+      learning_curve: 1,
+      duration: 1,
+      players: 1,
+      difficulty: 1,
+      cost: 1,
+    };
+  }
 }
 
-console.log(JSON.stringify(gamelist, null, 4))
+console.log(JSON.stringify(gamelist, null, 4));
