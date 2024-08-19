1d252bee75029687305b89ac10dd5a593d19f85e
Adrian Ortega <adrianog.sw@gmail.com>
2024-08-19 15:45:29 -0600

    Return a nice LOADING SPINNER component Fixes #36
    REACT Query always returns an "isLoading" variable. Make use
    of this variable to displaying a nice Loading spinner while data
    is being loaded by REACT Query

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,16 +1,20 @@
 import { useTopThree } from '../features/boardgame/useBoardGameList';
 import Button from './Button';
 import GameCard from './GameCard';
+import Loader from './Loader';
 import { Title } from './Title';
 
 function Home() {
   const { isLoading, topThree } = useTopThree();
   return (
     <div className="my-4 px-4 text-center">
+      {isLoading && <Loader />}
       <Title />
       <span className="mt-10 flex w-full justify-center text-8xl">👑</span>
       <div className="flex w-full justify-center py-10 font-sans text-4xl font-extrabold text-amber-300">
-        <span className='bg-black px-5 py-2 rounded-2xl bg-blend-color-burn border-slate-100 bg-opacity-20 border border-opacity-40'>Top 3 Board Games</span>
+        <span className="rounded-2xl border border-slate-100 border-opacity-40 bg-black bg-opacity-20 px-5 py-2 bg-blend-color-burn">
+          Top 3 Board Games
+        </span>
       </div>
       <div className="mb-12 flex w-full items-center justify-center">
         <div className="flex flex-wrap items-center justify-center">
