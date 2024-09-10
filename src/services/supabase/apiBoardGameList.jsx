255665cf7fd945a0d5ef4c81aa2366b6fcba5b00
elfus <elfus0.1@gmail.com>
2024-09-10 12:39:25 -0600

    Fixed data structure returned by supabase/apiBoardGameList.
    It should match the structure returned by
    the JSON server.

--- src/services/supabase/apiBoardGameList.jsx
+++ src/services/supabase/apiBoardGameList.jsx
@@ -51,7 +51,19 @@ function rank(games) {
 
 export async function getTopThree() {
   const supaGames = await supabase.from('ai_score').select();
+  
+  /* NOTE: The following code might look excessive, the reason
+  for it is that we are matching the structure provided by the JSON
+  server. Of course this chould have been avoided by making a better
+  design of the tables and endpoint APIs which could have simplified
+  this function, but please keep in mind that this project was
+  developed for learning purposes, in fact this comment turned out
+  to be the result of that learning process.
 
+  I will leave this function as is to match the JSON structure
+  as returned by the JSON Server. By doing this we avoid modifying
+  the code in GameCard.jsx
+  */
   let ratedG = supaGames.data.map((game) => ({
     ...game,
     overall: calculateOverall(game),
@@ -68,10 +80,29 @@ export async function getTopThree() {
 
   const games = [];
   for (let i = 0; i < 3; i++) {
-    games.push({ ...q.data[i], score: { ...ratedG[i] } });
+    games.push({ ...q.data[i], score: { 
+        fun : ratedG[i].fun,
+        learning_curve: ratedG[i].learning_curve,
+        duration: ratedG[i].duration ,
+        players:  ratedG[i].players,
+        difficulty:  ratedG[i].difficulty,
+        cost:  ratedG[i].cost
+      }, 
+      players : {
+        min:q.data[i].players_min ,
+        max:q.data[i].players_max ,
+      },
+      images: {
+        default:q.data[i].images_default,
+        '2x' :  q.data[i].images_2x,
+        banner: q.data[i].images_banner
+      },
+      overall: ratedG[i].overall,
+      rank: ratedG[i].rank
+      } 
+    );
   }
-  console.log(games);
-  // const ratedGames = await getBoardGamesPage({ page: 1, pageSize: 3 });
+  
   return games;
 }
 
