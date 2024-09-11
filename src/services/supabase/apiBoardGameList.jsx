398929f55ec142e3bf0022fd9861f682df01151b
Adrian Ortega <adrianog.sw@gmail.com>
2024-09-10 19:30:29 -0600

    Refactored code to normalize according to the new columns uploaded.
    Stop calculating values on the front end and just use whatever values we retrieved from the back end.

--- src/services/supabase/apiBoardGameList.jsx
+++ src/services/supabase/apiBoardGameList.jsx
@@ -2,6 +2,67 @@ import toast from 'react-hot-toast';
 import { BASE_API_URL } from '../apiConstants';
 import supabase from '../supabase';
 
+function normalize(games_table, ai_score_table) {
+  /* NOTE: The following code might look excessive, the reason
+    for it is that we are matching the structure provided by the JSON
+    server. Of course this chould have been avoided by making a better
+    design of the tables and endpoint APIs which could have simplified
+    this function, but please keep in mind that this project was
+    developed for learning purposes, in fact this comment turned out
+    to be the result of that learning process.
+
+    I will leave this function as is to match the JSON structure
+    as returned by the JSON Server. By doing this we avoid modifying
+    the code in GameCard.jsx
+
+    */
+  let map = new Map(
+    games_table.map((row) => [
+      row.id,
+      {
+        ...row,
+        score: {
+          fun: 0,
+          learning_curve: 0,
+          duration: 0,
+          players: 0,
+          difficulty: 9,
+          cost: 0,
+        },
+        players: {
+          min: 0,
+          max: 0,
+        },
+        images: {
+          default: '',
+          '2x': '',
+          banner: '',
+        },
+      },
+    ]),
+  );
+
+  for (const score_row of ai_score_table) {
+    let currGame = map.get(score_row.id);
+
+    map.set(score_row.id, {
+      ...currGame,
+      score: { ...score_row },
+      players: {
+        min: currGame.players_min,
+        max: currGame.players_max,
+      },
+      images: {
+        default: currGame.images_default,
+        '2x': currGame.images_2x,
+        banner: currGame.images_banner,
+      },
+    });
+  }
+
+  return Array.from(map.values());
+}
+
 export async function getBoardGameList() {
   const allGames = await fetch(`${BASE_API_URL}/games`).then((res) =>
     res.json(),
@@ -19,13 +80,28 @@ export async function getBoardGamesRanked() {
 }
 
 export async function getBoardGamesPage({ page, pageSize }) {
-  // This REST call will get ALL the board games
   if (!page || !pageSize) return null;
 
-  const gamesPage = await fetch(
-    `${BASE_API_URL}/games?_page=${page}&_limit=${pageSize}`,
-  ).then((res) => res.json());
-  return gamesPage;
+  const games = await supabase
+    .from('games')
+    .select('*')
+    .order('rank', { ascending: true })
+    .range((page - 1) * pageSize, page * pageSize - 1);
+
+  const ids = games.data.map((row) => row.id);
+  const ai_score = await supabase.from('ai_score').select().in('id', ids);
+  if (games.error) {
+    toast.error('Error fetching page data: ', games.error);
+    throw new Error(games.error);
+  }
+
+  if (ai_score.error) {
+    toast.error('Error fetching page data: ', ai_score.error);
+    throw new Error(ai_score.error);
+  }
+  const dataNormalized = await normalize(games.data, ai_score.data);
+
+  return dataNormalized;
 }
 
 export async function getBoardGamesCount() {
@@ -41,71 +117,13 @@ export async function getBoardGamesCount() {
   return count;
 }
 
-function calculateOverall(game) {
-  let sum = 0;
-  sum += game.difficulty;
-  sum += game.fun;
-  sum += game.learning_curve;
-  const propertyCount = 3;
-  return Number((sum / propertyCount).toFixed(2));
-}
-
-function rank(games) {
-  return games
-    .slice()
-    .sort((a, b) => b.overall - a.overall)
-    .map((game, index) => ({ ...game, rank: index + 1 }));
-}
-
 export async function getTopThree() {
-  const supaGames = await supabase.from('ai_score').select();
+  const supaGames = await supabase.from('games').select().lte('rank', 3);
+  const ids = supaGames.data.map((row) => row.id);
+  const score_table = await supabase.from('ai_score').select().in('id', ids);
+  let dataNormalized = normalize(supaGames.data, score_table.data);
 
-  /* NOTE: The following code might look excessive, the reason
-  for it is that we are matching the structure provided by the JSON
-  server. Of course this chould have been avoided by making a better
-  design of the tables and endpoint APIs which could have simplified
-  this function, but please keep in mind that this project was
-  developed for learning purposes, in fact this comment turned out
-  to be the result of that learning process.
-
-  I will leave this function as is to match the JSON structure
-  as returned by the JSON Server. By doing this we avoid modifying
-  the code in GameCard.jsx
-  */
-  let ratedG = supaGames.data.map((game) => ({
-    ...game,
-    overall: calculateOverall(game),
-  }));
-  ratedG = rank(ratedG);
-
-  ratedG
-    .slice()
-    .sort((a, b) => b.overall - a.overall)
-    .slice(0, 3);
-  const ids = ratedG.map((row) => row.id);
-
-  const q = await supabase.from('games').select().in('id', ids);
-
-  const games = [];
-  for (let i = 0; i < 3; i++) {
-    games.push({
-      ...q.data[i],
-      score: { ...ratedG[i] },
-      players: {
-        min: q.data[i].players_min,
-        max: q.data[i].players_max,
-      },
-      images: {
-        default: q.data[i].images_default,
-        '2x': q.data[i].images_2x,
-        banner: q.data[i].images_banner,
-      },
-      overall: ratedG[i].overall,
-      rank: ratedG[i].rank,
-    });
-  }
-
-  return games;
+  return dataNormalized;
 }
 
 export async function getLastUpdated() {
